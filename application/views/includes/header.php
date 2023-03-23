174add4f27ab63c41f256495a2cb904b235b1d3f
sayantandev-web <sayantan.ece.imps@gmail.com>
2023-03-24 01:33:56 +0530

    modified code

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -29,8 +29,9 @@
   <link rel="stylesheet" media="all" href="<?php echo base_url(); ?>assets/fontend/css/editstyle.css" />
   <style>
   .navbar ul {align-items: unset !important;}.l-nav-horizontal__brand {padding: 0 2rem !important;}.clients {padding: 0;background: linear-gradient(180deg, #032851 0%, #032851 100%);color: #fdc134;}.Scroll {height:600px;overflow-y: scroll;}.services .card-img img {margin-top: 30px;width: 100%;height : 250px;border-radius: 50%}.services .card-body {margin-top: 10px;text-align: justify;}#footer .footer-top {padding: 25px 0;}#footer .footer-bottom{padding-top: 5px;padding-bottom: 5px}.services .card-title {margin-top: 20px;}#footer .footer-top .social-links {margin-top: -50px;}.text-color {color: #fdc134;}.hover-shadow {transition: .3s ease;}.card {position: relative;display: -ms-flexbox;display: flex;-ms-flex-direction: column;flex-direction: column;min-width: 0;word-wrap: break-word;background-color: #fff;background-clip: border-box;border: 1px solid rgba(0,0,0,.125);border-radius: 0.25rem;}.rounded-0 {border-radius: 0!important;}.p-0 {padding: 0!important;}.border-primary {border-color: #ededf1 !important;}.mb-5, .my-5 {margin-bottom: 3rem!important;}.rounded-0 {border-radius: 0!important;}.card-img-top {width: 100%;border-top-left-radius: calc(0.25rem - 1px);border-top-right-radius: calc(0.25rem - 1px);}.card-body {-ms-flex: 1 1 auto;flex: 1 1 auto;padding: 1.25rem;}.mb-2, .my-2 {margin-bottom: 0.5rem!important;}.list-inline {padding-left: 0;list-style: none;}.list-inline-item:not(:last-child) {margin-right: 0.5rem;}.list-inline-item {display: inline-block;}.text-color {color: #5c5c77;}class^="ti-"], [class*=" ti-"] {font-family: 'themify';speak: none;font-style: normal;font-weight: normal;font-variant: normal;text-transform: none;line-height: 1;-webkit-font-smoothing: antialiased;-moz-osx-font-smoothing: grayscale;}.mr-1, .mx-1 {margin-right: 0.25rem!important;}.mb-4, .my-4 {margin-bottom: 1.5rem!important;}p, .paragraph {font-weight: 400;color: #5c5c77;font-size: 15px;line-height: 1.9;font-family: 'Raleway';}.btn:not(:disabled):not(.disabled) {cursor: pointer;}.btn-primary {background: #ffbc3b;color: #fff;}.btn-sm {font-size: 14px;padding: 10px 35px;}.btn {font-size: 16px;font-family: "Poppins", sans-serif;text-transform: capitalize;padding: 15px 40px;border-radius: 0;font-weight: 500;border: 0;position: relative;z-index: 1;transition: .2s ease;overflow: hidden;}.hover-shadow:hover {box-shadow: 0px 4px 25px 0px rgb(27 39 71 / 15%);}.school_img {width: 35%;float: left;position: relative;z-index: 9;margin-left: 2%;}.school_img img {width: 100%;float: left;}.into-inner {width: 60%;float: left;margin-top: 100px;}.intro {background: #ebebeb;padding: 10px 20px;position: relative;border-top-right-radius: 20px;border-bottom-right-radius: 20px;}.intro h3 {font-size: 35px !important;text-align: left;padding: 0;font-family: 'Raleway';font-weight: 700;color: #000;letter-spacing: 1px;margin: 0 !important;padding: 0;text-align: center;line-height: normal !important;}.intro p {color: #002a5c;font-size: 18px !important;padding: 0 0 4px;max-width: 900px;margin: 0 auto;text-align: center;}.intro p:nth-child(3) {color: black;font-style: initial;font-size: 18px;padding: 10px 0 0;max-width: 900px;margin: 0 auto;font-family: 'Helvetica', sans-serif;line-height: 25px;text-transform: capitalize;}.primary-nav li {width: 177px;}.about .content p {text-align: justify; font-family: 'Raleway';}.services .card img {transform: scale(1.1) !important;}.about .content h3{font-size: 32px !important;text-align: center;}#wowslider-container1 .ws-title{text-align: center;}.ws_controls{display: none;}
+
   @media (min-width: 1500px) and (max-width: 2560px)  {
-    .primary-nav li {width: 190px;}
+    .primary-nav li {width: 216px;}
   }
   .portfolio-details .portfolio-info {display: inline-block;}
   .portfolio-info {width: 100%}
@@ -45,6 +46,12 @@
   .dropdown-toggle::after{display: inline-block; margin-left: 0.255em; vertical-align: 1px; content: ""; border-top: 0.3em solid; border-right: 0.3em solid transparent;border-bottom: 0; border-left: 0.3em solid transparent;}
   .dropdown-menu{background: #fde9ac;}
   .dropdown-item:hover{background: #ffbf00;}
+  @media (min-width: 992px) and (max-width: 2560px)  {
+    .menu_sec {display: none;}
+  }
+  @media (max-width: 991px) {
+    .menu_sec {padding: 0;}
+  }
   </style>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
   <script>
@@ -54,8 +61,12 @@
       $(this).scrollTop(0);
 
       $("a").click(function(){
-            $("#menu").hide();    //or $("#menu").slideUp() if you want it to slide up instead of just disappearing.
-        });
+        $("#menu").hide();    //or $("#menu").slideUp() if you want it to slide up instead of just disappearing.
+      });
+
+      $(".navbar-toggler").click(function(){
+        $(".cstm_toggle").show();    //or $("#menu").slideUp() if you want it to slide up instead of just disappearing.
+      });
     });
   </script>
   <!-- Start WOWSlider.com HEAD section -->
@@ -68,7 +79,7 @@
 <?php $footer_content=$this->common_model->get_data(SETTINGS,array('id'=>1));?>
   <!-- ======= Header ======= -->
   <header class="l-header header">
-    <div class="mobile-bar">
+    <!-- <div class="mobile-bar">
       <div class="mobile-bar__nav-toggle">
         <button class="nav-toggle js-nav-toggle" aria-controls="primary-nav" aria-expanded="false" aria-label="Toggle Main Menu">
         <span class="nav-toggle__icon nav-toggle__icon--menu">Menu</span>
@@ -78,7 +89,7 @@
       <div class="mobile-bar__university">
         <a href="/"><img src="<?php echo base_url()?>uploads/site_logo/<?php echo $footer_content[0]['profile_pic'];?>" class="ucd-logo" alt="IIT Mandi Logo"/></a>
       </div>
-    </div>
+    </div> -->
     <div class="l-header__branding"></div>
     <div class="l-navbar header__navbar navbar menu--closed">
       <div class="l-container--navigation off-canvas off-canvas--left">
@@ -86,15 +97,6 @@
           <div class="l-nav-horizontal__brand">
             <a href="<?php echo base_url()?>"><img src="<?php echo base_url()?>uploads/site_logo/<?php echo $footer_content[0]['profile_pic'];?>" class="ucd-logo1" alt="IIT Mandi Logo" width="85" height="58" /></a>
           </div>
-          <!-- <div class="l-nav-horizontal__search-popup">
-            <div class="region region-search">
-              <button class="search-popup__open js-search-popup__open">
-              </button>
-              <div class="search-block-form search-popup" data-drupal-selector="search-block-form" id="block-ucd-search" role="search">
-                
-              </div>
-            </div>
-          </div> -->
           <div class="l-nav-horizontal__primary-nav">
             <div class="region region-navigation">
               <nav aria-labelledby="block-ucd-main-menu-menu" id="block-ucd-main-menu" class="primary-nav primary-nav--mega primary-nav--justify">
@@ -118,37 +120,9 @@
                       <li class="menu-item">
                         <a href="<?php echo base_url()?>pages/staff" data-drupal-link-system-path="node/2336">Staff</a>
                       </li>
-                      <!-- <li class="menu-item">
-                        <a href="<?php echo base_url()?>pages/students" data-drupal-link-system-path="node/2336">Students</a>
-                      </li>
-                      <li class="menu-item">
-                        <a href="<?php echo base_url()?>pages/technical_staff" data-drupal-link-system-path="node/2336">Technical Staff</a>
-                      </li>
-                      <li class="menu-item">
-                        <a href="<?php echo base_url()?>pages/supporting_staff" data-drupal-link-system-path="node/2336">Supporting Staff</a>
-                      </li> -->
                     </ul>
                   </li>
 
-                  <!-- <li class="menu-item menu-item--expanded">
-                    <div class="submenu-toggle__wrapper primary-nav__top-link">
-                      <a href="#" data-drupal-link-system-path="node/21">Academics<span class="primary-nav__submenu-indicator"></span>
-                      </a>
-                      <button class="submenu-toggle" aria-label="Toggle Submenu"><span class="submenu-toggle__icon"></span></button>
-                    </div>
-                    <ul class="menu">
-                      <li class="menu-item menu-item--expanded">
-                        <div class="submenu-toggle__wrapper ">
-                          <a href="#" title="Information about admission to IIT Mandi for prospective undergraduates" data-drupal-link-system-path="node/2241">Specialization<span class="primary-nav__submenu-indicator"></span>
-                          </a>
-                          <button class="submenu-toggle" aria-label="Toggle Submenu"><span class="submenu-toggle__icon"></span></button>
-                        </div>
-                      </li>
-                      <li class="menu-item">
-                        <a href="#" data-drupal-link-system-path="node/49441">Programs</a>
-                      </li>
-                    </ul>
-                  </li> -->
                   <li class="menu-item menu-item--expanded">
                     <div class="submenu-toggle__wrapper primary-nav__top-link">
                       <a href="#" data-drupal-link-system-path="node/16">Facilities<span class="primary-nav__submenu-indicator"></span>
@@ -178,9 +152,6 @@
                       <li class="menu-item">
                         <a href="https://nirmaan.iitmandi.co.in/" data-drupal-link-system-path="node/2330">Nirmaan Club</a>
                       </li>
-                      <!-- <li class="menu-item">
-                        <a href="#" data-drupal-link-system-path="node/2098">Student Activities</a>
-                      </li> -->
                     </ul>
                   </li>
                   <li class="menu-item menu-item--expanded">
@@ -229,21 +200,6 @@
                       </li>
                     </ul>
                   </li>
-                  <!-- <li class="menu-item menu-item--expanded">
-                    <div class="submenu-toggle__wrapper primary-nav__top-link">
-                      <a href="javascript:void(0)" data-drupal-link-system-path="node/2343">Log In<span class="primary-nav__submenu-indicator"></span>
-                      </a>
-                      <button class="submenu-toggle" aria-label="Toggle Submenu"><span class="submenu-toggle__icon"></span></button>
-                    </div>
-                    <ul class="menu">
-                      <li class="menu-item">
-                        <a href="<?php echo base_url()?>student" data-drupal-link-system-path="student">Student Login</a>
-                      </li>
-                      <li class="menu-item">
-                        <a href="<?php echo base_url()?>faculty" data-drupal-link-system-path="faculty">Faculty Login</a>
-                      </li>
-                    </ul>
-                  </li> -->
                   <li class="menu-item menu-item--expanded">
                     <div class="submenu-toggle__wrapper primary-nav__top-link">
                       <a href="<?php echo base_url()?>" style="padding: 0;"><img src="<?php echo base_url()?>uploads/site_logo/1666640517228772.png" class="ucd-logo1" alt="IIT Mandi Logo" width="85" height="58" /></a>
@@ -264,10 +220,10 @@
         <div class="col-sm-12">
           <nav class="navbar navbar-expand-lg navbar-light">
             <div class="container-fluid">
-              <a class="navbar-brand" href="#"><img src="logo.png" alt=""></a>
+              <a class="navbar-brand" href="#"><img src="<?php echo base_url()?>uploads/site_logo/<?php echo $footer_content[0]['profile_pic'];?>" class="ucd-logo1" alt="IIT Mandi Logo" width="85" height="58" /></a>
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
               <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0 cstm_toggle">
                   <li class="nav-item">
                     <a class="nav-link active dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" aria-current="page" >People</a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
