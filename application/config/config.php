f8d671a1b9f426c5ef1b3b6237815a8ee489bce8
Subhankar-Redapple <subhankar@redappletech.com>
2023-04-15 00:37:09 +0530

    update1

--- application/config/config.php
+++ application/config/config.php
@@ -25,7 +25,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  */
 
  
-$config['base_url'] = 'https://scene.iitmandi.ac.in/dev';
+$config['base_url'] = 'http://localhost/mandi/iitmandi/';
 
 /*
 |--------------------------------------------------------------------------
