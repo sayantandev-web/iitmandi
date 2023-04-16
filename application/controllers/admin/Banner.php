0c44e69e8cce82a67f8b81d3c9110a1ddc4947c6
sayantandev-web <sayantan.ece.imps@gmail.com>
2023-04-16 17:50:17 +0530

    latest code

--- application/controllers/admin/Banner.php
+++ application/controllers/admin/Banner.php
@@ -6,7 +6,7 @@ class Banner extends CI_Controller{
 		@parent::__construct();
 		$this->load->library('pagination');
 		$this->load->library('image_lib');
-		session_start();
+		//session_start();
         if($this->session->userdata('uid') == ''){
             redirect(base_url().'admin/');
         }
